9329cc752069e636df8d70b7e3f5baea649b98f0
Wuan21 <qn21012004@gmail.com>
2025-03-24 13:44:31 +0700

    [Add] lab-05 base64

---
+++ lab-05/base64/encrypt.py
@@ -0,0 +1,13 @@
+import base64
+
+def main():
+    input_string = input("Nhập thông tin cần mã hoá: ")
+    
+    encoded_bytes = base64.b64encode(input_string.encode("utf-8"))
+    encoded_string = str(encoded_bytes, "utf-8")
+    with open("data.txt", "w") as file:
+        file.write(encoded_string)
+    print(f"Đã mã hoá thông tin và lưu vào file data.txt")
+    
+if __name__ == "__main__":
+    main()
